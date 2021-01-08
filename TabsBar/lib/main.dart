62b6504d3848235bbea82ad39c1a4e44f2e1f8fb
José Nildo de Macedo Pereira <josenildodeveloper@gmail.com>
2021-01-08 13:25:29 -0300

    ADD: Adicionar Guia, mudar visual geral
    Criado uma terceira guia
    Exibido um texto ao mudar de guia
    Mudado a cor de fundo do container ao selecionar uma guia

--- TabsBar/lib/main.dart
+++ TabsBar/lib/main.dart
@@ -8,25 +8,53 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: DefaultTabController(
-        length: 2,
+        length: 3,
         child: Scaffold(
           appBar: AppBar(
-            title: Text("AppBar com guias"),
+            title: Text("Tabs Demo"),
             bottom: TabBar(
-              tabs: <Widget>[
+              tabs: [
                 Tab(
-                  icon: Icon(Icons.android),
+                  icon: Icon(Icons.directions_car),
                 ),
                 Tab(
-                  icon: Icon(Icons.cloud_download),
+                  icon: Icon(Icons.directions_transit),
                 ),
+                Tab(
+                  icon: Icon(Icons.directions_bike),
+                )
               ],
             ),
           ),
           body: TabBarView(
-            children: <Widget>[
-              Text('Primeira guia selecionada'),
-              Text('Segunda guia selecionada'),
+            children: [
+              Container(
+                color: Colors.deepOrangeAccent,
+                child: Center(
+                  child: Text(
+                    'Primeira Guia',
+                    style: TextStyle(),
+                  ),
+                ),
+              ),
+              Container(
+                color: Colors.blueGrey,
+                child: Center(
+                  child: Text(
+                    'Segunda Guia',
+                    style: TextStyle(),
+                  ),
+                ),
+              ),
+              Container(
+                color: Colors.teal,
+                child: Center(
+                  child: Text(
+                    'Terceira Guia',
+                    style: TextStyle(),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
