391ee498a1ee2ede2ff812e71c4f0418332f276a
José Nildo de Macedo Pereira <josenildodeveloper@gmail.com>
2021-01-28 15:36:05 -0300

    ADD: Criar validação de email
    Criado um metodo para validar o campo de email afim de detectar se um email invalido foi inserido

--- FormularioComValidacao/lib/main.dart
+++ FormularioComValidacao/lib/main.dart
@@ -98,4 +98,17 @@ class _MeuFormState extends State<MeuForm> {
     }
     return null;
   }
+
+  String _validarEmail(String value) {
+    String pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regExp = new RegExp(pattern);
+    if (value.length == 0) {
+      return "Informe o Email";
+    } else if (!regExp.hasMatch(value)) {
+      return "Email inválido";
+    } else {
+      return null;
+    }
+  }
 }
