b08cc663a411d7e2a8c10f76a4ef6ed9a177eb8b
José Nildo de Macedo Pereira <josenildodeveloper@gmail.com>
2021-01-28 09:59:38 -0300

    ADD: Criar um Form com GlobayKey
    Criação de um container Form para agrupar e validar vários campos de formmulario
    Criação de uma GlobalKey unica para identificação futura do formulario para sua validação

--- FormularioComValidacao/lib/main.dart
+++ FormularioComValidacao/lib/main.dart
@@ -1,64 +1,21 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MeuForm());
 
-class MyApp extends StatelessWidget {
+class MeuForm extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Formulário com Validação',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _MeuFormState createState() => _MeuFormState();
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class _MeuFormState extends State<MeuForm> {
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+    // Cria o widget Form usando _formKey
+    return Form(
+      key: _formKey,
+      child: ,
     );
   }
 }
